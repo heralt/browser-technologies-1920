f8b38bb89ca171024756d5b3fe469b3d942231a5
heralt <heralt@live.nl>
2020-07-03 00:59:24 +0200

    init project

--- enquete_app/app.js
+++ enquete_app/app.js
@@ -4,72 +4,64 @@ const port = 3000;
 const fetch = require('node-fetch');
 const parser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
 
-app.use(express.static('static'));
-app.use(parser.urlencoded({extended:true}));
-app.set('views','views');
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(parser.urlencoded({extended: true}));
+app.set('views', 'views');
 app.set('view engine', 'ejs');
 
-const write_to_file = (file,data) => {
-    fs.writeFile(file, JSON.stringify(data, null, 2), err => {
-        if(err) console.log(err);
-        console.log(err);
-    });
-};
+const JSON_FILE = './data.json';
 
-const read_from_file = () => {
-
-};
-
-const data = (req, res) => {
-
-    let index = -1;
-    const{naam, stdnt_nr, studie} = req.body;
-
-    console.log(req.key)
-
-    const json_file = './data.json';
-    fs.readFile(json_file, (err, data) => {
-        if(err) return console.log(err);
+function putData(req) {
+    const {naam, stdnt_nr, studie, goals, duur_doel, keuze, toekomst} = req.body;
+    fs.readFile(JSON_FILE, async (err, data) => {
+        if (err) return console.log(err);
         const content_json = JSON.parse(data);
-
-        const student = content_json.data.find( ({stdnt_nr},i) =>
-        {
-            if(stdnt_nr === req.body.stdnt_nr){
-                index = i;
-                return i;
-            }
-        });
-
-        if(student){
-            content_json.data[index-1].random = "random";
-            write_to_file(json_file,content_json);
-        } else {
-            const form_data = {stdnt_nr, naam};
-            content_json.data.push(form_data);
-            write_to_file(json_file,content_json);
-        }
+        const form_data = {stdnt_nr, naam, studie, goals, duur_doel, keuze, toekomst};
+        content_json.data.push(form_data);
+        write_to_file(JSON_FILE, content_json);
     });
+}
 
-};
+function write_to_file(file, data) {
+    fs.writeFile(file, JSON.stringify(data, null, 2), err => {
+        if (err) console.log(err);
+    });
+}
+
+function getData(index, vraag, res, url) {
+    fs.readFile(JSON_FILE, async (err, data) => {
+        if (err) return console.log(err);
+        const content_json = JSON.parse(data);
+        res.render(vraag, {
+            content: content_json.data[index],
+            continueSurvey: `http://localhost:3000${url}`,
+        })
+    });
+}
 
 app.get('/', (req, res) => {
     res.render('init_page');
-    std_nummer = parseInt(req.body.stdnt_nr);
 });
 
-app.post('/vraag/:id', (req, res) => {
-    data(req,res);
-    res.render(`vraag${req.params.id}`);
+app.post('/:vraag', (req, res) => {
+    let url = req.originalUrl;
+    putData(req);
+    res.render(req.params.vraag, {
+        content: '',
+        continueSurvey: `http://localhost:3000${url}`,
+    });
 });
 
-app.get('/vraag/:id', (req, res) => {
-    data(req,res);
-    res.render(`vraag${req.params.id}`);
+app.get('/:vraag', (req, res) => {
+    let url = req.originalUrl;
+    let value = parseInt(url.match(/\d+/)[0]);
+    getData(value, req.params.vraag, res, url);
 });
 
-app.post('/vraag/:submit', (req, res) => {
-    res.render(`vraag${req.params.id}`);
+app.get('/submit', (req,res) => {
+
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
---
+++ enquete_app/static/js/main.js
@@ -0,0 +1,17 @@
+let vraag = window.location.pathname.split('/');
+
+function checkStorage(value){
+    if(window.localStorage.getItem(value)){
+
+    } else {
+        window.localStorage.setItem(value, )
+    }
+}
+
+
+
+switch (vraag) {
+    case 1:
+        checkStorage(vraag);
+}
+window.location.pathname
